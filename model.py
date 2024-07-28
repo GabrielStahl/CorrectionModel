0a6d97d8acf97992f74f2d6fdc48b4917101dfe9
GabrielStahl <47419421+GabrielStahl@users.noreply.github.com>
2024-07-28 13:06:47 +0100

    reduce model depth to further reduce memory consumtion

--- model.py
+++ model.py
@@ -24,16 +24,13 @@ class CorrectionUNet(nn.Module):
         self.enc5 = self.conv_block(128, 256, 1)
         self.enc6 = self.conv_block(256, 256, 1)
         self.enc7 = self.conv_block(256, 256, 1)
-        self.enc8 = self.conv_block(256, 256, 1)
-        self.enc9 = self.conv_block(256, 256, 1)
 
         # Decoder (upsampling)
         self.dec1 = self.conv_block(256 + 256, 256, 2, dropout=self.dropout)
         self.dec2 = self.conv_block(256 + 128, 128, 2, dropout=self.dropout)
         self.dec3 = self.conv_block(128 + 64, 64, 2)
         self.dec4 = self.conv_block(64 + 32, 32, 2)
-        self.dec5 = self.conv_block(32 + 16, 16, 2)
-        self.dec6 = nn.Conv3d(16, out_channels, kernel_size=1)
+        self.dec5 = nn.Conv3d(32, out_channels, kernel_size=1)
 
     def conv_block(self, in_channels, out_channels, num_conv, dropout=None):
         layers = []
@@ -57,16 +54,13 @@ class CorrectionUNet(nn.Module):
         enc5 = self.enc5(self.maxpool(enc4))
         enc6 = self.enc6(enc5)
         enc7 = self.enc7(enc6)
-        enc8 = self.enc8(enc7)
-        enc9 = self.enc9(enc8)
 
         # Decoder
-        dec1 = self.dec1(torch.cat([self.upsample(enc9, enc6.size()), enc6], dim=1))
-        dec2 = self.dec2(torch.cat([self.upsample(dec1, enc5.size()), enc5], dim=1))
-        dec3 = self.dec3(torch.cat([self.upsample(dec2, enc4.size()), enc4], dim=1))
-        dec4 = self.dec4(torch.cat([self.upsample(dec3, enc3.size()), enc3], dim=1))
-        dec5 = self.dec5(torch.cat([self.upsample(dec4, enc2.size()), enc2], dim=1))
-        output = self.dec6(self.upsample(dec5, x.size()))
+        dec1 = self.dec1(torch.cat([self.upsample(enc7, enc6.size()), enc6], dim=1))
+        dec2 = self.dec2(torch.cat([self.upsample(dec1, enc4.size()), enc4], dim=1))
+        dec3 = self.dec3(torch.cat([self.upsample(dec2, enc3.size()), enc3], dim=1))
+        dec4 = self.dec4(torch.cat([self.upsample(dec3, enc2.size()), enc2], dim=1))
+        output = self.dec5(self.upsample(dec4, x.size()))
 
         return output
 
