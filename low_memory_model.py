2f9a2a8b3882857ec13feef9554656d677fb891c
GabrielStahl <47419421+GabrielStahl@users.noreply.github.com>
2024-07-31 20:42:47 +0100

    Update low_memory_model.py

--- low_memory_model.py
+++ low_memory_model.py
@@ -14,34 +14,39 @@ warnings.filterwarnings("ignore")
 class UltraLightCorrectionUNet(nn.Module):
     def __init__(self, in_channels=3, out_channels=5):
         super(UltraLightCorrectionUNet, self).__init__()
-
+        
         # Encoder (downsampling)
-        self.enc1 = self.conv_block(in_channels, 4)
-        self.enc2 = self.conv_block(4, 8)
-        self.enc3 = self.conv_block(8, 16)
-
+        self.enc1 = self.conv_block(in_channels, 16)
+        self.enc2 = self.conv_block(16, 32)
+        self.enc3 = self.conv_block(32, 64)
+        self.enc4 = self.conv_block(64, 128)
+        
         # Decoder (upsampling)
-        self.dec2 = self.conv_block(16 + 8, 8)
-        self.dec1 = self.conv_block(8 + 4, 4)
-        self.final_conv = nn.Conv3d(4, out_channels, kernel_size=1)
+        self.dec3 = self.conv_block(128 + 64, 64)
+        self.dec2 = self.conv_block(64 + 32, 32)
+        self.dec1 = self.conv_block(32 + 16, 16)
+        
+        self.final_conv = nn.Conv3d(16, out_channels, kernel_size=1)
 
     def conv_block(self, in_channels, out_channels):
         return nn.Sequential(
             nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
-    
+
     def forward(self, x):
         # Encoder
         enc1 = self.enc1(x)
         enc2 = self.enc2(self.maxpool(enc1))
         enc3 = self.enc3(self.maxpool(enc2))
-
+        enc4 = self.enc4(self.maxpool(enc3))
+        
         # Decoder
-        dec2 = self.dec2(torch.cat([self.upsample(enc3, enc2.size()), enc2], dim=1))
+        dec3 = self.dec3(torch.cat([self.upsample(enc4, enc3.size()), enc3], dim=1))
+        dec2 = self.dec2(torch.cat([self.upsample(dec3, enc2.size()), enc2], dim=1))
         dec1 = self.dec1(torch.cat([self.upsample(dec2, enc1.size()), enc1], dim=1))
+        
         output = self.final_conv(dec1)
-
         return output
 
     def maxpool(self, x):
